2d25256a17d51d7ff7e6cdcbc0098e2662b0aab0
Emma Lopez <25lopezev@rbhs208.net>
2024-06-24 20:44:41 -0500

    Initial Commit

---
+++ Conocerte/ConocerteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConocerteApp.swift
+//  Conocerte
+//
+//  Created by Emma Lopez on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct ConocerteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
